0b9aecd6faad1f2d71caed1dfccbbc910bdc8e3c
fardog <nate@milkandtang.com>
2017-09-04 11:16:03 -0700

    fixup ts definitions for new api

--- index.d.ts
+++ index.d.ts
@@ -6,20 +6,28 @@ export as namespace randomLib
 export = randomLib
 
 declare namespace randomLib {
-    export function randomFloat(): number
-    export function randomFloat(ready: ReadyFn): void
-    export function randomFloats(opts: MultiAsyncOptions, ready: MultiReadyFn): void
-    export function randomFloats(ready: MultiReadyFn): void
-    export function randomFloats(): number[]
-    export function randomFloats(opts?: MultiOptions): number[]
-    export function randomInt(opts: IntOptions, ready: ReadyFn): void
-    export function randomInt(ready: ReadyFn): void
-    export function randomInt(opts?: IntOptions): number
-    export function randomInts(opts: MultiAsyncIntOptions, ready: MultiReadyFn): void
-    export function randomInts(ready: MultiReadyFn): void
-    export function randomInts(opts?: MultiIntOptions): number[]
+    export function float(): Promise<number>
+    export function float(ready: ReadyFn): void
 
-    export function promise(p?: PromiseConstructorLike): Promisified
+    export function int(opts?: IntOptions): Promise<number>
+    export function int(opts: IntOptions, ready: ReadyFn): void
+    export function int(ready: ReadyFn): void
+
+    export function floats(opts?: MultiAsyncOptions): Promise<number[]>
+    export function floats(opts: MultiAsyncOptions, ready: MultiReadyFn): void
+    export function floats(ready: MultiReadyFn): void
+
+    export function ints(opts?: MultiAsyncIntOptions): Promise<number[]>
+    export function ints(opts: MultiAsyncIntOptions, ready: MultiReadyFn): void
+    export function ints(ready: MultiReadyFn): void
+
+    export function floatSync(): number
+
+    export function intSync(opts?: IntOptions): number
+
+    export function floatsSync(opts?: MultiOptions): number[]
+
+    export function intsSync(opts?: MultiIntOptions): number[]
 
     export interface MultiOptions {
         num?: number
@@ -38,11 +46,4 @@ declare namespace randomLib {
 
     export type ReadyFn = (err: Error, num: number) => void
     export type MultiReadyFn = (err: Error, nums: number[]) => void
-
-    export interface Promisified {
-        randomFloat(): Promise<number>
-        randomFloats(opts?: MultiAsyncOptions): Promise<number[]>
-        randomInt(opts?: MultiIntOptions): Promise<number>
-        randomInts(opts?: MultiAsyncIntOptions): Promise<number[]>
-    }
 }
